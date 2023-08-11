3f338a617fb1aaa097ef685109b58ba328b4a6fd
Ogunwomoju <tboysammy@gmail.com>
2023-08-10 18:32:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - A program that print a line with puts function
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-return (0);
+         puts("\"programming is like building a multilingual puzzle");
+         return (0);
 }
