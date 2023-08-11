4fcda3f86ca97ae39e68454abe60a3d1db396bc5
Ogunwomoju <tboysammy@gmail.com>
2023-08-10 18:47:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - A program that print a line with puts function
+ * main - Entry point
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-         puts("\"programming is like building a multilingual puzzle");
-         return (0);
+        puts("\"programming is like building a multilingual puzzle");
+        return (0);
 }
