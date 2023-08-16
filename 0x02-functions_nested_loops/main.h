1f806b9607b939a9d4d29ec6e063fd364769069b
Ogunwomoju <tboysammy@gmail.com>
2023-08-16 04:15:26 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,13 @@
-#ifinder main.h
-#define main.h
-
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _is lower(int c);
+int _islower(int c);
 int _isalpha(int c);
-int print_sign(int 0);
+int print_sign(int);
 int _abs(int);
 int  print_last_digit(int);
-void jack_bauer(voi);
+void jack_bauer(void);
 void time_table(void);
-in add(int, int);
+int add(int, int);
 void print_to_98(int n);
 void print_time_table(int n);
-
-#end i|f
