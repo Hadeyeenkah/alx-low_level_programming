1e97244f1897aa0ab1cb3d4ef7b9a41efdfdc2d4
Ogunwomoju <tboysammy@gmail.com>
2023-08-16 03:32:36 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,15 @@
 
 /**
  * add - adds two integers and returns the result
- * @a: int to be added to b 
+ * @a: int to be added to b
  * @b: int to be added to a
- * Return: sum of a and b 
+ * Return: sum of a and b
  */
 int add(int a, int b)
 {
 	int c;
-	
-	c = a = b;
+
+	c = a + b;
+
 	return (c);
 }
--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,25 +1,27 @@
 /*
- * file: 101-natural.c
+ * File: 101-natural.c
+ * Auth: vincent kip
  */
 
 #include <stdio.h>
 
 /**
- * main - list all the natural number below 1024 (excluded)
- *        that are multiple of 3 or 5.
+ * main - Lists all the natural numbers below 1024 (excluded)
+ *        that are multiples of 3 or 5.
  *
  * Return: Always 0.
- *        */
+ */
 int main(void)
 {
 	int i, sum = 0;
-	for (i = 0 ; i < 1024; i++)
+
+	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3) == 0 || (i % 5) == 0
+		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
 	}
 
-	printf("%d\", sum);
-	
+	printf("%d\n", sum);
+
 	return (0);
 }
