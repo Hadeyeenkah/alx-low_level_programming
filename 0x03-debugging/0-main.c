8296b71d2aed264b6f7543b6ef6563be305676c7
Ogunwomoju <tboysammy@gmail.com>
2023-08-17 00:07:30 -0700

    C-Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Tes t function positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive _or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void postitive_or_negative(int i);
+int larges_number(int a, int b, int c);
+void print_remaining_days(inta,int b, int c);
+void print_remaining_days (int month, int days, int years);
+
+#endif
