c3bb20858a993aa82cddc6ab7f5bb9f778b428b6
Ogunwomoju <tboysammy@gmail.com>
2023-08-18 13:56:12 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_numbers - print numbers from 0 to 9
  * Return: void
@@ -9,6 +10,6 @@ void print_numbers(void)
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
-		_putchar(i + "0');
+		_putchar(i + '0');
 	_putchar('\n');
 }
