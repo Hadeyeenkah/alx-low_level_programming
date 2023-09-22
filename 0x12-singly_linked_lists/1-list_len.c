05635e16eaa0bfb2ac0745fe494f6e9e27cb6a64
Ogunwomoju <tboysammy@gmail.com>
2023-09-21 17:11:53 -0700

    linklist

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,17 +1,19 @@
 #include "lists.h"
-/** *list_len - returns then number of elements in a list. 
- * *@y: singly linked list.
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
  *
- *Return: number of elements in the list.
+ * Return: number of elements in h
  */
-size_t list_len(const list_t *y)
+size_t list_len(const list_t *h)
 {
-	size_t bytes;
-	bytes = 0;
-	while (y != NULL)
-	{
-		y = y->next;
-		bytes++;
-	}
-	return (bytes);
+ size_t n = 0;
+
+ while (h)
+ {
+ n++;
+ h = h->next;
+ }
+ return (n);
 }
